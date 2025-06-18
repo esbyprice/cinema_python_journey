d50e1b80ad036a9c3aa6328b8d5e2ce1ceb8945d
Sam Price <sam.price92@gmail.com>
2025-06-18 15:10:32 -0700

    added additiona practice py page

---
+++ morepractice.py
@@ -0,0 +1,13 @@
+#Definitions
+FPS=24
+WB=5500
+is_tungsten=False
+
+
+#Operators
+print("FPS", FPS)
+print("White Balance", WB)
+if is_tungsten:
+    print("Change White balance")
+else:
+    print("Keep WB")
