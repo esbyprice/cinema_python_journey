4a39df8d0e355f7391cb93dc421f406ff4cb0c3f
Sam Price <sam.price92@gmail.com>
2025-07-06 18:16:21 -0700

    day 3 bonus

---
+++ day3_bonusexercise.py
@@ -0,0 +1,7 @@
+def exposure_value(shutter, aperature, ISO):
+    try:
+        ev = (aperature ** 2) / shutter * (100 / ISO)
+        return round(ev,2)
+    except Exception as e:
+        return f"Error: {e}"
+print(exposure_value(1/125, 2.8, dog))
