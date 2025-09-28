9bafaf0a546ab45c274c7fb7a52d161c7e462478
Sam Price <sam.price92@gmail.com>
2025-09-27 17:19:59 -0700

    new additions shit code that doesn't work

--- wk2_dy2_exercise1.py
+++ wk2_dy2_exercise1.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+now=datetime.now()
 class CameraSetting:
     def __init__(self, iso, shutter, aperture):
         self.iso=iso
@@ -10,7 +12,7 @@ class CameraSetting:
 class CameraRig:
     def __init__(self, name):
         self.name=name
-        self.settings=[]
+        self.settings=[] 
     
     def add_setting(self, setting):
         self.settings.append(setting)
@@ -21,13 +23,13 @@ class CameraRig:
 
     def save_rig(self):
         with open("wk2_dy2_log.txt", "a") as file:
-            file.write 
+            file.write(f"{now}" + {CameraRig.settings} + "\n") 
 
 
-macro=CameraSetting(400,"1/200", "f/2.8")
+macro=CameraSetting(412300,"1/23200", "f/2.23238")
 studio=CameraSetting(800,"1/400", "f/4")
 
 rig1=CameraRig("Macro")
 rig1.add_setting(macro)
-rig1.add_setting(studio)
+rig1.save_rig()
 rig1.list_settings()
